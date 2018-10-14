e8cd10c7ec094f37d3b7dd5313535ed8f474618e
FabianHaeubl <43788035+FabianHaeubl@users.noreply.github.com>
2018-10-14 17:44:01 +0200

    Add files via upload

---
+++ 8-10-2018_objekte/app_kon.js
@@ -0,0 +1,9 @@
+//Consrtuctor Function
+function Circle (radius){
+    console.log('this', this);
+    this.radius = radius;
+    this.draw = function(){
+        console.log('draw');
+    };
+}
+const another = new Circle(1);
